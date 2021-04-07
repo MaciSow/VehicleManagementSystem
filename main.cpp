180f2d6cad94ce7734eb2e8233df00ad8fa9b242
Maciej Sowiński <>
2021-04-07 22:54:01 +0200

    Add SFML library

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include <SFML/Window.hpp>
+
+using namespace sf;
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    Window window(sf::VideoMode(800, 600), "Vehicle Management System");
+
+    while (window.isOpen()) {
+        Event event;
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+    }
     return 0;
 }
+
